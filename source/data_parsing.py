f39363388ac9fd38dcfc93da7fa249e7e08dc205
Victor <victor.wangkun@gmail.com>
2016-07-20 00:56:28 -0700

    Merged 3 csv files; To-do: map phone brand and model to numeric values

--- source/data_parsing.py
+++ source/data_parsing.py
@@ -7,60 +7,57 @@ Created on Mon Jul 18 22:51:16 2016
 
 from __future__ import print_function, division
 import pandas as pd
+import numpy as np
 
+peek = True
 
-def test_load(filepath):
-	
-	df = pd.read_csv(filepath)
-	print(df.head())
-	print(df.describe())
-		
-	
-def main_load():
-	
-	file1 = '../input/gender_age_train.csv'
-	print('\n', file1)
-	test_load(file1)
-					
-	file2 = '../input/gender_age_test.csv'
-	print('\n', file2)
-	test_load(file2)
-	
-	file3 = '../input/phone_brand_device_model.csv'
-	print('\n', file3)
-	test_load(file3)
-	
-	file4 = '../input/events.csv'
-	print('\n', file4)
-	test_load(file4)
-	
-	
-	
-def test_join(file1, file2):
-	
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
-	
-    df = df1.merge(df2, on='device_id', how='left')
-    df.to_csv('../processed_data/gender_age_phone_brand_device_model.csv')
-	
-    print(df.head())
+# === load 1 ===
+file1 = '../input/gender_age_train.csv'
+if peek: print('\n', file1)
+gender_age_train = pd.read_csv(file1, encoding='utf-8', dtype={"device_id": np.str})
+if peek: 
+    print(gender_age_train.head())
+    print(gender_age_train["device_id"].nunique())
+    
+file2 = '../input/events.csv'
+if peek: print('\n', file2)
+events = pd.read_csv(file2, encoding='utf-8', dtype={"event_id": np.str, "device_id": np.str}, 
+                         parse_dates=["timestamp"])
+if peek: 
+    print(events.head())
+    print(events["device_id"].nunique())
+
+# === merge 1 ===
+if peek: print('\n === Merge 1 ===')
+cleaned_data = gender_age_train.merge(events, on='device_id', how='inner')
+if peek: 
+    print(cleaned_data.head())
+    print(cleaned_data["device_id"].nunique())
+
+del gender_age_train, events
+    
+
+# === load 2 ===
+file3 = '../input/phone_brand_device_model.csv'
+if peek: print('\n', file3)
+phone_brand_device_model = pd.read_csv(file3, encoding='utf-8', dtype={"device_id": np.str})
+if peek: 
+    print(phone_brand_device_model.head())
+    print(phone_brand_device_model["device_id"].nunique())
+    
+# === merge 2 ===
+if peek: print('\n === merge 2 ===')
+cleaned_data = cleaned_data.merge(phone_brand_device_model, on='device_id', how='left')
+if peek: 
+    print(cleaned_data.head())
+    print(cleaned_data["device_id"].nunique())
+    
+##### To-do: map phone brand and model to numeric numbers #####
+    
+#file4 = '../input/app_events.csv'
+#app_events = pd.read_csv(file4, encoding='utf-8', dtype={"event_id": np.str, "app_id": np.str})
+
+        
+
  
 
-def main_join():
-	
-	file1 = '../input/gender_age_train.csv'
-	file2 = '../input/phone_brand_device_model.csv'
-#	file3 = './input/events.csv'
-	
-	test_join(file1, file2)	
- 
- 
- 
-#	test_join(file1, file3)
-	
-	
-	
-if __name__ == '__main__':
-#	main_load()
-    main_join()
